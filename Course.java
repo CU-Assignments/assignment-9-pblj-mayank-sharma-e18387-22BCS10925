af1efd88d4d4a05b42ef7ce728fdf69a1b8bd469
22BCS10925 <22BCS10925@cuchd.in>
2025-04-24 17:40:32 +0530

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,18 @@
+public class Course {
+    String courseName;
+    int duration;
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
+ 
